614d926b5bac9a6b393ae40403754c418f6c3be7
Daria Kondakova <d.d.kondakova@gmail.com>
2023-05-05 18:16:46 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import json
 import os
 import streamlit as st
 
-st.text_input(label, 
+st.text_input('Who', 
               value="", 
               max_chars=None, 
               key="subject", 
@@ -16,7 +16,7 @@ st.text_input(label,
               placeholder='Zeus', disabled=False, 
               label_visibility="visible")
 
-st.text_input(label, 
+st.text_input('Does what', 
               value="", 
               max_chars=None, 
               key="predicate", 
@@ -28,7 +28,7 @@ st.text_input(label,
               label_visibility="visible")
 
 
-st.text_input(label, 
+st.text_input('To whom', 
               value="", 
               max_chars=None, 
               key="object", 
