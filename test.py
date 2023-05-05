4ace1ec7d5833cc58f224a7a72d02f75ab086b76
Daria Kondakova <d.d.kondakova@gmail.com>
2023-05-05 18:28:12 +0200

    Update test.py

--- test.py
+++ test.py
@@ -7,39 +7,41 @@ import streamlit as st
 
 st.title('Enter hylemes below')
 
-st.text_input('Who', 
-              value="", 
-              max_chars=None, 
-              key="subject", 
-              type="default", 
-              help=None, 
-              autocomplete=None, 
-              on_change=None, 
-              placeholder='Zeus', disabled=False, 
-              label_visibility="visible")
-
-st.text_input('Does what', 
-              value="", 
-              max_chars=None, 
-              key="predicate", 
-              type="default", 
-              help=None, 
-              autocomplete=None, 
-              on_change=None, 
-              placeholder='sees', disabled=False, 
-              label_visibility="visible")
-
-
-st.text_input('To whom', 
-              value="", 
-              max_chars=None, 
-              key="object", 
-              type="default", 
-              help=None, 
-              autocomplete=None, 
-              on_change=None, 
-              placeholder='Leda', disabled=False, 
-              label_visibility="visible")
+with st.form(key='columns_in_form'):
+  c1, c2, c3 = st.columns(3)
+    with c1:
+      subj = st.text_input('Who', 
+                  value="", 
+                  max_chars=None, 
+                  key="subject", 
+                  type="default", 
+                  help=None, 
+                  autocomplete=None, 
+                  on_change=None, 
+                  placeholder='Zeus', disabled=False, 
+                  label_visibility="visible")
+    with c2:
+      pred = st.text_input('Does what', 
+                  value="", 
+                  max_chars=None, 
+                  key="predicate", 
+                  type="default", 
+                  help=None, 
+                  autocomplete=None, 
+                  on_change=None, 
+                  placeholder='sees', disabled=False, 
+                  label_visibility="visible")
+    with c3:
+      obj = st.text_input('To whom', 
+                  value="", 
+                  max_chars=None, 
+                  key="object", 
+                  type="default", 
+                  help=None, 
+                  autocomplete=None, 
+                  on_change=None, 
+                  placeholder='Leda', disabled=False, 
+                  label_visibility="visible")
 
 if st.button('Next hyleme'):
     st.write('Ok!')
