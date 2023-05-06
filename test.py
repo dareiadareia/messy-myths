e39ab16a2f28595eea89783806d1e0cee99be863
dareia <d.d.kondakova@gmail.com>
2023-05-06 09:51:13 +0200

    changed to slider widget

--- test.py
+++ test.py
@@ -8,8 +8,7 @@ import streamlit as st
 
 st.title('Enter hylemes below')
 
-if 'n_rows' not in st.session_state:
-    st.session_state.n_rows = 1
+num_rows = st.slider('Number of Rows', min_value=1,max_value=10,value=1)
 
 if 'data' not in st.session_state:
     data = pd.DataFrame({'subject':[],'predicate':[],'object':[]})
@@ -23,51 +22,44 @@ def save_data():
             'predicate':[st.session_state.predicate],
             'object':[st.session_state.object]})
     st.session_state.data = pd.concat([st.session_state.data, row])
-    st.session_state.n_rows = 0
 
-if st.session_state.n_rows > 0:
-    form = st.form(clear_on_submit=True, key='input_form'+str(st.session_state.n_rows))
-    with form:
-        c1, c2, c3 = st.columns(3)
-        for i in range(1, st.session_state.n_rows+1):
-            with c1:
-              subj = st.text_input('Who', 
-                          value="", 
-                          max_chars=None, 
-                          key="subject"+str(i), 
-                          type="default", 
-                          help=None, 
-                          autocomplete=None, 
-                          on_change=None, 
-                          placeholder='Zeus', disabled=False, 
-                          label_visibility="visible")
-            with c2:
-              pred = st.text_input('Does what', 
-                          value="", 
-                          max_chars=None, 
-                          key="predicate"+str(i), 
-                          type="default", 
-                          help=None, 
-                          autocomplete=None, 
-                          on_change=None, 
-                          placeholder='sees', disabled=False, 
-                          label_visibility="visible")
-            with c3:
-              obj = st.text_input('To whom', 
-                          value="", 
-                          max_chars=None, 
-                          key="object"+str(i), 
-                          type="default", 
-                          help=None, 
-                          autocomplete=None, 
-                          on_change=None, 
-                          placeholder='Leda', disabled=False, 
-                          label_visibility="visible")
-        submit = st.form_submit_button("Save sequence", on_click=save_data)
+form = st.form(clear_on_submit=True, key='input_form'+str(st.session_state.n_rows))
+with form:
+    c1, c2, c3 = st.columns(3)
+    for i in range(1, num_rows+1):
+        with c1:
+          subj = st.text_input('Who', 
+                      value="", 
+                      max_chars=None, 
+                      key="subject"+str(i), 
+                      type="default", 
+                      help=None, 
+                      autocomplete=None, 
+                      on_change=None, 
+                      placeholder='Zeus', disabled=False, 
+                      label_visibility="visible")
+        with c2:
+          pred = st.text_input('Does what', 
+                      value="", 
+                      max_chars=None, 
+                      key="predicate"+str(i), 
+                      type="default", 
+                      help=None, 
+                      autocomplete=None, 
+                      on_change=None, 
+                      placeholder='sees', disabled=False, 
+                      label_visibility="visible")
+        with c3:
+          obj = st.text_input('To whom', 
+                      value="", 
+                      max_chars=None, 
+                      key="object"+str(i), 
+                      type="default", 
+                      help=None, 
+                      autocomplete=None, 
+                      on_change=None, 
+                      placeholder='Leda', disabled=False, 
+                      label_visibility="visible")
+    submit = st.form_submit_button("Save sequence", on_click=save_data)
 
 st.dataframe(data) 
-
-add_row = st.button(label="add")
-if add_row:
-    st.session_state.n_rows += 1
-    st.experimental_rerun()
