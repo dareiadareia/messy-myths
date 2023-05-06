adbd3e65e18a08a47d3d720a10b1d987e8f8c165
dareia <d.d.kondakova@gmail.com>
2023-05-06 09:57:08 +0200

    fixing

--- test.py
+++ test.py
@@ -24,7 +24,7 @@ def save_data(num_rows):
                 'object':[st.session_state.object+str(i)]})
     st.session_state.data = pd.concat([st.session_state.data, row])
 
-form = st.form(clear_on_submit=True, key='input_form'+str(st.session_state.n_rows))
+form = st.form(clear_on_submit=True, key='input_form')
 with form:
     c1, c2, c3 = st.columns(3)
     for i in range(1, num_rows+1):
