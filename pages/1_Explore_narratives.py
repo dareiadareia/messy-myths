b94a5603761cfb231f81b8ef8bfac2d5f421c20c
dareia <d.d.kondakova@gmail.com>
2023-05-17 15:33:32 +0200

    fixing things

--- pages/1_Explore_narratives.py
+++ pages/1_Explore_narratives.py
@@ -9,7 +9,7 @@ directory_data = 'data'
 
 list_of_seqs = []
 for filename in os.listdir(directory_data):
-	f = os.path.join(directory, filename)
+	f = os.path.join(directory_data, filename)
 	if os.path.isfile(f):
 		with open(f) as file:
 			list_of_seqs.append(json.loads(file.read()))
@@ -20,6 +20,7 @@ selected = st.selectbox('Choose a story to compare',
 	multiple_choice,
 	key = narrative1,
 	)
+
 
 
 #for seq in list_of_seqs:
