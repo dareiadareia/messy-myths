ca6d010bfd46165cfc2f719d59984b3e230e55e0
dareia <d.d.kondakova@gmail.com>
2023-05-18 16:48:28 +0200

    adding multiple comparison

--- pages/1_Explore_narratives.py
+++ pages/1_Explore_narratives.py
@@ -14,12 +14,18 @@ for filename in os.listdir(directory_data):
 		with open(f) as file:
 			list_of_seqs.append(json.loads(file.read()))
 
-multiple_choice = [seq["title"] + f'({seq["passage reference"]})' for seq in list_of_seqs]
+number_of_narratives = st.number_input('Number of narratives to compare')
 
-selected = st.selectbox('Choose a story to compare',
+selected_narratives = []
+for i in number_of_narratives:
+	selected_narratives.append(st.selectbox('Choose a story to compare',
 	multiple_choice,
 	#key = narrative1,
 	)
+		)
+
+multiple_choice = [seq["title"] + f'({seq["passage reference"]})' for seq in list_of_seqs]
+
 
 for seq in list_of_seqs:
 	if seq["title"] + f'({seq["passage reference"]})' == selected:
