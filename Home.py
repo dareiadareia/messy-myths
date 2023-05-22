1315e8ed05d59cabd8fce5cf59be303d593327ff
dareia <d.d.kondakova@gmail.com>
2023-05-22 10:25:51 +0200

    minor stuff

--- Home.py
+++ Home.py
@@ -3,3 +3,6 @@ import streamlit as st
 st.title('Messy myths: how can we compare mythological narratives?')
 
 st.markdown('[under construction...]')
+
+if st.button('Show plans'):
+	st.markdown('- link to other projects on digital mythology - calculate similarity score – make tables editable by the user ')
