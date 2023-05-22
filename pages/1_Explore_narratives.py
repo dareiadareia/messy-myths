842de825f364ea7fd9e66a7e2444b85ec83610dd
dareia <d.d.kondakova@gmail.com>
2023-05-22 09:12:11 +0200

    fixing

--- pages/1_Explore_narratives.py
+++ pages/1_Explore_narratives.py
@@ -232,8 +232,8 @@ def compare_narratives(seq1, seq2, crit): # crit is a list, seq1 and seq2 are di
 				"predicate2": "", 
 				"object2": ""})
 		# i+=1
-	new_hyl_seq += stack1
-	new_hyl_seq += stack2
+	# new_hyl_seq += stack1
+	# new_hyl_seq += stack2
 	print(new_hyl_seq)
 	return new_hyl_seq
 
