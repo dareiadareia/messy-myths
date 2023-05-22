bec42c8e598ec0ec865a318eb6f2d1f0f5097131
dareia <d.d.kondakova@gmail.com>
2023-05-22 10:28:18 +0200

    minor stuff

--- Home.py
+++ Home.py
@@ -2,7 +2,11 @@ import streamlit as st
 
 st.title('Messy myths: how can we compare mythological narratives?')
 
+st.markdown('Terminology: *hylemes* are elements of a narrative consisting of a subject, a predicate, and an object. *Hyleme sequences* are ordered lists of *hylemes* that represent the narrative.')
+
 st.markdown('[under construction...]')
 
 if st.button('Show plans'):
-	st.markdown('- link to other projects on digital mythology - calculate similarity score – make tables editable by the user ')
+	st.markdown('- link to other projects on digital mythology')
+	st.markdown('- calculate similarity score')
+	st.markdown('– make tables editable by the user')
