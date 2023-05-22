8c6d731de9b83aa95a50b850c0127bac8ad2f871
dareia <d.d.kondakova@gmail.com>
2023-05-22 08:08:52 +0200

    fixing alignment

--- pages/1_Explore_narratives.py
+++ pages/1_Explore_narratives.py
@@ -182,6 +182,7 @@ def compare_narratives(seq1, seq2, crit): # crit is a list, seq1 and seq2 are di
 	new_hyl_seq = []
 	for elem in hyl_seq1:
 		if compare_hylemes(elem, hyl_seq2[i], crit):
+			new_hyl_seq.append(stack)
 			new_hyl_seq.append({
 				"subject1": elem["subject"], 
 				"predicate1": elem["predicate"], 
@@ -190,14 +191,15 @@ def compare_narratives(seq1, seq2, crit): # crit is a list, seq1 and seq2 are di
 				"predicate2": hyl_seq2[i]["predicate"], 
 				"object2": hyl_seq2[i]["object"]
 				})
+			stack=[]
 		else:
-			new_hyl_seq.append({"subject1": elem["subject"], 
-				"predicate1": elem["predicate"], 
-				"object1": elem["object"],
-				"subject2": "", 
-				"predicate2": "", 
-				"object2": ""})
-			new_hyl_seq.append({
+			# new_hyl_seq.append({"subject1": elem["subject"], 
+				# "predicate1": elem["predicate"], 
+				# "object1": elem["object"],
+				# "subject2": "", 
+				# "predicate2": "", 
+				# "object2": ""})
+			stack.append({
 				"subject1": "", 
 				"predicate1": "", 
 				"object1": "",
