40cd8ac8a41a34af986b33260f3a5f51ffd2a768
dareia <d.d.kondakova@gmail.com>
2023-05-22 09:46:30 +0200

    better labels

--- pages/1_Explore_narratives.py
+++ pages/1_Explore_narratives.py
@@ -109,14 +109,14 @@ with col1:
 		compare_obj = st.checkbox('object', key='checkbox_obj')
 		submitted1 = st.form_submit_button("Save")
 with col2:
-	st.write('If you want to treat entities/actions as identical for this comparison, select them here.')
+	# st.write('If you want to treat entities/actions as identical for this comparison, select them here.')
 	currently_same = []
 	form2 = st.form(key='entities_to_be_same')
 	with form2:
-		'Entities:'
+		# 'Entities:'
 		subcol1, subcol2, subcol3 =  st.columns([3,1,3])
 		with subcol1:
-			st.selectbox('', entities,
+			st.selectbox('Entities to be treated as equal', entities,
 			key='entity1', label_visibility='collapsed') 
 		with subcol2:
 			'=' 
@@ -128,10 +128,10 @@ with col2:
 		submitted3 = st.form_submit_button("Save")
 	form3 = st.form(key='actions_to_be_same')
 	with form3:
-		'Actions:'
+		# 'Actions:'
 		subcol4, subcol5, subcol6 =  st.columns([3,1,3])
 		with subcol4:
-			st.selectbox('', 
+			st.selectbox('Actions to be treated as equal:', 
 			actions, 
 			key='action1', label_visibility='collapsed') 
 		with subcol5:
