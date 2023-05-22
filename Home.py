4f5db14982d360629a3d40b49d28f4fbf97a8d0e
dareia <d.d.kondakova@gmail.com>
2023-05-22 10:29:47 +0200

    minor stuff

--- Home.py
+++ Home.py
@@ -4,9 +4,9 @@ st.title('Messy myths: how can we compare mythological narratives?')
 
 st.markdown('Terminology: **hylemes** are elements of a narrative consisting of a subject, a predicate, and an object. **Hyleme sequences** are ordered lists of *hylemes* that represent the narrative.')
 
-st.markdown('[under construction...]')
+# st.markdown('[]')
 
-if st.button('Show plans'):
+if st.button('Under construction...'):
 	st.markdown('- link to other projects on digital mythology')
 	st.markdown('- calculate similarity score')
-	st.markdown('– make tables editable by the user')
+	st.markdown('- make tables editable by the user')
