557a4979e9934434a3bff93dca2ed3c86e670a0d
dareia <d.d.kondakova@gmail.com>
2023-05-24 17:32:15 +0200

    fixing

--- pages/1_Explore_narratives.py
+++ pages/1_Explore_narratives.py
@@ -173,8 +173,8 @@ comparison_str = ', '.join(comparison_criteria)
 def display_equalities(same_stuff):
 	if len(st.session_state['same_'+same_stuff]) > 0:
 		temp = [f'{t[0]} = {t[1]}' for t in st.session_state[same_stuff]]
-			for i in temp:
-				st.write('- ' + i)
+		for i in temp:
+			st.write('- ' + i)
 	else:
 		st.write('None')
 
