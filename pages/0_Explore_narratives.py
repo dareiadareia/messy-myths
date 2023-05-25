62e304e1a338647cb5e36a93826b2fe1636ada95
dareia <d.d.kondakova@gmail.com>
2023-05-25 17:34:50 +0200

    added the page to browse through narratives

---
+++ pages/0_Explore_narratives.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import os
+import json
+import datetime
+# from st_aggrid import AgGr
+
+path_to_data = 'sequence.json'
+
+with open(path_to_data) as file:
+	list_of_seqs = json.loads(file.read())
+
+human_references = [f'{seq["passage reference"]}: {seq["title"]}' for seq in list_of_seqs]
+
+selected_narrative_human = st.selectbox("Select a narrative to display:",
+	human_references
+	)
+
+selected_narrative = [seq for seq in list_of_seqs if f'{seq["passage reference"]}: {seq["title"]}' == selected_narrative_human]
+
+st.table(pd.DataFrame.from_records(selected_narrative[0]["hyleme sequence"]))
