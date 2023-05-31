eafc209a2b018a7dfe1a95ed87f52797bc8375a5
dareia <d.d.kondakova@gmail.com>
2023-05-31 22:27:13 +0200

    added more API exception code

--- save_to_github.py
+++ save_to_github.py
@@ -23,7 +23,7 @@ def push_to_repo_branch(file_or_variable, gitHubFileName, fileName, repo_slug, b
     :return None
     :raises Exception: if file with the specified name cannot be found in the repo
     '''
-    
+    api_exception = False
     message = "Automated update " + str(datetime.datetime.now())
     path = "https://api.github.com/repos/%s/branches/%s" % (repo_slug, branch)
 
@@ -63,58 +63,60 @@ def push_to_repo_branch(file_or_variable, gitHubFileName, fileName, repo_slug, b
                 print(type(old_content))
             except KeyError:
                 st.write(':red[**There seems to be a problem with the API. Wait a bit and try again, please.**]')
+                api_exception = True
                 break
 
+    if not api_exception:
     # if sha is None after the for loop, we did not find the file name!
-    if sha is None:
-        print("Could not find " + gitHubFileName + " in repos 'tree' ")
-        raise Exception
-
-    if file_or_variable == 'file':
-        with open(fileName) as data:
-            print(data)
-            content = base64.b64encode(data.read().encode())
-    elif file_or_variable == 'variable':
-        #print(fileName)
-        #print(type(fileName))
-        new_content = fileName
-        #new_content = json.dumps(fileName)#.encode()
-    else:
-        print('Wrong parameter (file_or_variable)!')
-
-    # gathered all the data, now let's push
-    inputdata = {}
-    inputdata["path"] = gitHubFileName
-    inputdata["branch"] = branch
-    inputdata["message"] = message
-    if file_or_variable == 'file':
-        inputdata["content"] = content.decode('utf-8')
-    if file_or_variable == 'variable':
-        print('-'*10)
-        print(old_content)
-        print(f'old content type is {type(old_content)}')
-        print(new_content)
-        print(f'new content type is {type(new_content)}')
-        temp = json.dumps(old_content + new_content)
-        #temp = ast.literal_eval(old_content.decode('utf-8')) + ast.literal_eval(new_content.decode('utf-8'))
-        print(f'Input data content is: {temp}')
-        #content = base64.b64encode(old_content + new_content)
-        inputdata["content"] = base64.b64encode(temp.encode()).decode('utf-8')
-    if sha:
-        inputdata["sha"] = str(sha)
-    print(inputdata)
-    print(json.dumps(inputdata))
-
-    updateURL = "https://api.github.com/repos/{0}/contents/{1}".format(repo_slug, gitHubFileName)
-    try:
-        rPut = requests.put(updateURL, auth=(user,token), data = json.dumps(inputdata))
-        if not rPut.ok:
-            print("Error when pushing to %s" % updateURL)
-            print("Reason: %s [%d]" % (rPut.text, rPut.status_code))
+        if sha is None:
+            print("Could not find " + gitHubFileName + " in repos 'tree' ")
             raise Exception
-    except requests.exceptions.RequestException as e:
-        print('Something went wrong! I will print all the information that is available so you can figure out what happend!')
-        print(rPut)
-        print(rPut.headers)
-        print(rPut.text)
-        print(e)
+
+        if file_or_variable == 'file':
+            with open(fileName) as data:
+                print(data)
+                content = base64.b64encode(data.read().encode())
+        elif file_or_variable == 'variable':
+            #print(fileName)
+            #print(type(fileName))
+            new_content = fileName
+            #new_content = json.dumps(fileName)#.encode()
+        else:
+            print('Wrong parameter (file_or_variable)!')
+
+        # gathered all the data, now let's push
+        inputdata = {}
+        inputdata["path"] = gitHubFileName
+        inputdata["branch"] = branch
+        inputdata["message"] = message
+        if file_or_variable == 'file':
+            inputdata["content"] = content.decode('utf-8')
+        if file_or_variable == 'variable':
+            print('-'*10)
+            print(old_content)
+            print(f'old content type is {type(old_content)}')
+            print(new_content)
+            print(f'new content type is {type(new_content)}')
+            temp = json.dumps(old_content + new_content)
+            #temp = ast.literal_eval(old_content.decode('utf-8')) + ast.literal_eval(new_content.decode('utf-8'))
+            print(f'Input data content is: {temp}')
+            #content = base64.b64encode(old_content + new_content)
+            inputdata["content"] = base64.b64encode(temp.encode()).decode('utf-8')
+        if sha:
+            inputdata["sha"] = str(sha)
+        print(inputdata)
+        print(json.dumps(inputdata))
+
+        updateURL = "https://api.github.com/repos/{0}/contents/{1}".format(repo_slug, gitHubFileName)
+        try:
+            rPut = requests.put(updateURL, auth=(user,token), data = json.dumps(inputdata))
+            if not rPut.ok:
+                print("Error when pushing to %s" % updateURL)
+                print("Reason: %s [%d]" % (rPut.text, rPut.status_code))
+                raise Exception
+        except requests.exceptions.RequestException as e:
+            print('Something went wrong! I will print all the information that is available so you can figure out what happend!')
+            print(rPut)
+            print(rPut.headers)
+            print(rPut.text)
+            print(e)
