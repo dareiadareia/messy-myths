35546a85367b1676946e1451f7de92a06b61ad3e
dareia <d.d.kondakova@gmail.com>
2023-05-31 20:39:08 +0200

    fixed explore narratives paths

--- pages/0_Explore_narratives.py
+++ pages/0_Explore_narratives.py
@@ -5,7 +5,7 @@ import json
 import datetime
 # from st_aggrid import AgGr
 
-path_to_data = 'sequence.json'
+directory_data = 'data'
 
 def show_narrative(sequence):
 	for k, v in sequence["metadata"].items():
@@ -14,9 +14,12 @@ def show_narrative(sequence):
 	clean_hyleme_sequence = pd.DataFrame.from_records(sequence["hyleme sequence"]).applymap(lambda x: x[0])
 	st.table(clean_hyleme_sequence)
 
-
-with open(path_to_data) as file:
-	list_of_seqs = json.loads(file.read())
+list_of_seqs = []
+for filename in os.listdir(directory_data):
+	f = os.path.join(directory_data, filename)
+	if os.path.isfile(f):
+		with open(f) as file:
+			list_of_seqs += json.loads(file.read())
 
 human_references = [f'{seq["metadata"]["passage reference"]}: {seq["metadata"]["title"]}' for seq in list_of_seqs]
 
