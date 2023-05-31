d12bb2a304e72c3c44ada47f3f13246096d51ae6
dareia <d.d.kondakova@gmail.com>
2023-05-31 19:48:39 +0200

    trying

--- pages/2_Enter_new_hyleme_sequence.py
+++ pages/2_Enter_new_hyleme_sequence.py
@@ -90,7 +90,8 @@ if presubmit:
 sequence_dict = {}
 hyl_sequence = st.session_state.data.to_dict('records')
 for k, v in hyl_sequence.items():
-    hyl_sequence[k] = v[0]
+    print(v)
+    # hyl_sequence[k] = v[0]
 sequence_dict['hyleme sequence'] = hyl_sequence
 sequence_dict['metadata'] = st.session_state.metadata
 
