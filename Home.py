97d7fa0f861dec30d14c2ba815f94a4738adcc3b
dareia <d.d.kondakova@gmail.com>
2023-05-31 21:05:40 +0200

    deleted test_function

--- Home.py
+++ Home.py
@@ -9,6 +9,3 @@ st.markdown('Terminology: **hylemes** are elements of a narrative consisting of 
 if st.button('Under construction...'):
 	st.markdown('- link to other projects on digital mythology')
 	st.markdown('- calculate similarity score')
-
-def test_function(s):
-	st.write(s)
