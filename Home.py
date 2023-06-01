03f65194d3a66e5b6547a2eb933ca06b411efdc3
dareia <d.d.kondakova@gmail.com>
2023-06-01 07:44:27 +0200

    readme

--- Home.py
+++ Home.py
@@ -2,7 +2,11 @@ import streamlit as st
 
 st.title('Messy myths: how can we compare mythological narratives?')
 
-st.markdown('Terminology: **hylemes** are elements of a narrative consisting of a subject, a predicate, and an object. **Hyleme sequences** are ordered lists of *hylemes* that represent the narrative.')
+
+st.markdown('This is a tool to work with mythological narratives using the _hylistic_ method as defined by [Zgoll 2019]. You can add your own narratives and compare them with others in the collection.')
+st.markdown('Some basic terminology: - **hylemes** are elements of a narrative consisting of a subject, a predicate, and an object; - **hyleme sequences** are ordered lists of *hylemes* that represent a narrative.')
+
+st.write('[Zgoll 2019]: Christian Zgoll, Tractatus Mythologicus, MythoS (Berlin; Boston: de Gruyter, 2019). https://doi.org/10.1515/9783110541588')
 
 # st.markdown('[]')
 
@@ -10,4 +14,3 @@ st.markdown('Terminology: **hylemes** are elements of a narrative consisting of 
 # 	st.markdown('- link to other projects on digital mythology')
 # 	st.markdown('- calculate similarity score')
 
-'Contact: dk@dareia.one' 
